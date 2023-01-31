32e01ce49f3f3f3b75c35330ccdb60bf6fbd9982
Enes Sahin <enesshahn@gmail.com>
2023-01-31 15:47:42 +0300

    Refactoring

--- Component/Light/Light.cs
+++ Component/Light/Light.cs
@@ -2,16 +2,20 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace RayTracingEngine
+namespace EnesShahn
 {
-	abstract class Light : Component
-	{
-		protected Color01 color;
-		public Color01 Color {
-			get { return color; }
-		}
+    namespace RayTracingEngine
+    {
+        abstract class Light : Component
+        {
+            protected Color01 color;
+            public Color01 Color
+            {
+                get { return color; }
+            }
 
-		public abstract Vector3 GetDirection();
-		public abstract float GetIntensity();
-	}
+            public abstract Vector3 GetDirection();
+            public abstract float GetIntensity();
+        }
+    }
 }
